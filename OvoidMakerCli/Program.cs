c9e12e35f7d5ff4eac9da0598e9e761b5f734f3c
IntangibleMatter <intangiblematter@intangiblematter.net>
2025-11-16 17:28:31 -0800

    Add CLI

---
+++ OvoidMakerCli/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace OvoidMakerCli;
+
+using LibOvoidMaker;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Hello, world!");
+		Console.WriteLine(OvoidMaker.Create(128, 128));
+	}
+}
